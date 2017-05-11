9c25aef8659cd8ee9ca34d159c76027f1991103a
qwqw8910 <qw5827898@gmail.com>
2017-05-11 23:29:38 +0800

    doc報表練習
    成功

---
+++ doc報表列印/WebSite1/dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [text1] NCHAR(10) NULL, 
+    [text2] NCHAR(10) NULL, 
+    [text3] NCHAR(10) NULL, 
+    [text4] NCHAR(10) NULL, 
+    [text5] NCHAR(10) NULL, 
+    [text6] NCHAR(10) NULL, 
+    [text7] NCHAR(10) NULL, 
+    [text8] NCHAR(10) NULL
+)
